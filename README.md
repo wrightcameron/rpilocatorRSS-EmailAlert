63c774c069afa2a3ae0b57f06c0c6e1f8f003670
Cameron Wright <ccameronwright@gmail.com>
2023-01-16 19:08:28 -0700

    Updated README, improved handling regions and models, and added requirements file.

--- README.md
+++ README.md
@@ -8,11 +8,43 @@ Simple Python RSS feed tracker for piLocator
 
 ## Setup
 
-Run `make` in root dir with Makefile, to clean dir `make clean`
+A Python virtual environment doesn't have to be used, but is recommend.  Otherwise jump to run after installing required pip packages.
+
+```bash
+python -m venv ./pilocatorEnv; \
+source ./pilocatorEnv/bin/activate; \
+pip install -r requirements.txt
+```
 
 ## Running
 
+Script is run with the following optional parameters `python run.py [-h] [-a AGE] [-m MODEL] [-r REGION] [-t TRIAL]`, use `-h` flag for help and more information.
 
+### Automating
+
+Use Cron to have this script automatically run whenever you desire.  Though run aggressively and piLocator will block access to the site.  In their words, "If you are looking to create a notification system for **personal use**, please use the RSS feed and don't hit the site more than **once a minute**. High frequency bots will get blocked." I do 4 times a day, each 6 hours apart.
+
+To access cron, use the command `crontab -l` to view your user cronjobs, and `crontab -e` to edit them.
+
+```cron
+0 */6 * * *Path/To/Repository/run.py
+```
+
+## Testing
+
+### Linting
+
+Python linting uses pylint and flake8.
+
+A general pylint check will use following pylint command `pylint $(git ls-files '*.py');`
+
+A general flake8 check will check for everything.  But for continuous integration the command used will be `flake8 . --count --show-source --statistics --exclude flaskEnv/`
+
+```bash
+flake8 --exclude flaskEnv/
+```
+
+Linting can also be done with Python Black, `python black <file>`
 
 ## References
 
--- run.py
+++ run.py
@@ -1,54 +1,67 @@
 import argparse
 import feedparser
-import re
 import datetime
+import smtplib
+from email.mime.text import MIMEText
+from dateutil import parser
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-def isDateOld(dateStr: str) -> bool:
+
+
+def isDateOld(dateStr: str, dateRange: int) -> bool:
     # Mon, 16 Jan 2023 11:54:13 GMT
-    from dateutil import parser
     date = parser.parse(dateStr)
     now = datetime.datetime.now(datetime.timezone.utc)
     dateDelta = now - date
-    return dateDelta.days > AGE
+    return dateDelta.days > dateRange
 
-# TODO Model and Region functions are same, combine them.
-def checkRegion(region: str) -> bool:
-    if REGION is None:
-        return True
-    p = re.search(f'({REGION})', region)
-    return p is None
-
-def checkModel(model: str) -> bool:
-    if MODEL is None:
-        return True
-    p = re.search(f'({MODEL})', model)
-    return p is None
-
-def checkConditions():
-    pass
+def sendEmail(subject, body):
+    load_dotenv()
+    emailSender = os.getenv('emailSender')
+    emailSenderServer = os.getenv('emailSenderServer')
+    emailSenderPassword = os.getenv('emailSenderPassword')
+    emailReciever = os.getenv('emailReciever')
+    
+    msg = MIMEText(body)
+    msg['Subject'] = subject
+    msg['From'] = emailSender
+    msg['To'] = emailReciever
+    smtp_server = smtplib.SMTP_SSL(emailSenderServer, 465)
+    smtp_server.login(emailSender, emailSenderPassword)
+    smtp_server.sendmail(emailSender, emailReciever, msg.as_string())
+    smtp_server.quit()
 
 def sendEmail():
     pass
 
-def run() -> None:
+def run(region: str, catagory: str, dateRange: int, isTrial: bool = False) -> None:
     # Get piLocatorFeed
-    piLocatorFeed = feedparser.parse("https://rpilocator.com/feed/")
+    #TODO What if we want to view all regions or all catagories now?
+    piLocatorFeed = feedparser.parse(f"https://rpilocator.com/feed/?country={region}&cat={catagory}")
 
     for entry in piLocatorFeed.entries:
         # TODO Maybe move these satements to a log
         # Determine if the pi is model, region, and date match
-        if isDateOld(entry.published):
+        if isDateOld(entry.published, dateRange):
             # RSS Feed is ordered by date, so if date is old enough break loop
             break
-        if checkRegion(entry.title) or checkModel(entry.title):
-            continue
         print(entry.published)
         print("******")
         print(entry.summary)
         print("------News Link--------")
         print(entry.link)
         # If pi is in stock send email notification
+        subject = "Email Subject"
+        body = "This is the body of the text message"
+        if not isTrial:
+            sendEmail(subject, body)
+        else:
+            print("Trial run, not sending email.")
         # TODO What if we have more than one entry, we still want one email
         # TODO Have way to prevent duplicate email from being sent, most likely temp file.
 
@@ -56,16 +69,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="PiLocatorRSSAlert: RSS Feed Reader Alert Script. Send email when desirable Pi found."
     )
-    parser.add_argument("-a", "--age", type=int, help="The amount of day's old an alert can be.")
-    parser.add_argument("-m", "--model", type=str, help="Pi Model to alert for.")
-    parser.add_argument("-r", "--region", type=str, help="Region the Pi is sold from.")
+    # TODO Default should be all regions and all catagories instead of US and CM4.  It's just easier for me right now
+    parser.add_argument("-a", "--age", type=int, help="The amount of day's old an alert can be.", default=0)
+    parser.add_argument("-m", "--model", type=str, help="Pi Model to alert for.", default='US')
+    parser.add_argument("-r", "--region", type=str, help="Region the Pi is sold from.", default='CM4')
+    parser.add_argument("-t", "--trial", type=bool, help="Run script without pushing any potential emails", default=False)
     args = parser.parse_args()
 
-    global AGE 
-    global MODEL
-    global REGION
-
-    AGE = args.age
-    MODEL = args.model
-    REGION = args.region
-    run()
+    run(args.region, args.model, args.age, args.trial)
